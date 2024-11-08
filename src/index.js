03ec59284def5de226e02500a5ea82386de28f97
betalantz <betalantz@gmail.com>
2024-11-08 09:10:11 -0800

    changes renderHi text

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function renderHi() {
     const h1 = document.createElement("h1");
-    h1.textContent = "HI!"
+    h1.textContent = "Hello world!"
     document.querySelector("body").append(h1)
 
 }
 
 renderHi()
 
-const noConflict = true
+const noConflict = false
