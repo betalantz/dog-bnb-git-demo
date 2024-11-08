6cb16c94cba98230f32bd5daf6472e5996e31e84
betalantz <betalantz@gmail.com>
2024-11-08 08:45:27 -0800

    adds renderHI()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log("DOM loaded")
+
+function renderHi() {
+    const h1 = document.createElement("h1");
+    h1.textContent = "HI!"
+    document.querySelector("body").append(h1)
+
+}
+
+renderHi()
